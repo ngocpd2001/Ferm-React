5d17cda3c24f6f3010d0df67841dce689ea19371
ngocpd2001 <phanngoc2213@gmail.com>
2023-10-02 13:50:08 +0700

    test

--- src/shared/Players.js
+++ src/shared/Players.js
@@ -36,7 +36,7 @@ const Players = [
   },
   {
     id: 6,
-    name: "Haaland 123git ",
+    name: "Haaland ",
     club: "MU",
     img: "assets/images/haaland.jpg",
     info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
