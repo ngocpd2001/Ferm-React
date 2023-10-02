8530e0860d33d2415c2a44f368bd70ae8dbd395c
ngocpd2001 <phanngoc2213@gmail.com>
2023-10-02 14:20:08 +0700

    test2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,40 @@
-import  React,{ Component } from "react"
-
-
+import React, { Component } from "react";
 
 export default function Navigation() {
-      return(
-      
-        <nav className="navbar navbar-dark navbar-expand-sm fixed-top">
-        <button className="navbar-toggler" type="button" data-toggle="collapse"
-        data-target="#Navbar">
+  return (
+    <nav className="navbar navbar-dark navbar-expand-sm fixed-top">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#Navbar"
+      >
         <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="Navbar">
+      </button>
+      <div className="collapse navbar-collapse" id="Navbar">
         <ul className="navbar-nav mr-auto">
-                <li className="nav-item active"><a className="nav-link" href="#home">
-                <span className="fa fa-home fa-lg"></span>Home</a></li>
-                <li className="nav-item"><a className="nav-link"  href="#new">
-                <span className="fa fa-list fa-lg"></span>New</a></li>
-                <li className="nav-item"><a className="nav-link"  href="#about">
-                <span className="fa fa-info fa-lg"></span>About</a></li>
-                <li className="nav-item"><a className="nav-link"  href="#contact">
-                <span className="fa fa-address-card fa-lg"></span>Contact</a></li>
-            </ul>
-
-        </div>
-            
-        </nav>
-      
-  
-      )
-   
-  }
+          <li className="nav-item active">
+            <a className="nav-link" href="#home">
+              <span className="fa fa-home fa-lg"></span>Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#new">
+              <span className="fa fa-list fa-lg"></span>New
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              <span className="fa fa-info fa-lg"></span>About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">
+              <span className="fa fa-address-card fa-lg"></span>Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,67 +1,63 @@
 import React, { Component } from "react";
 
-import Players from "../shared/Players"
+import Players from "../shared/Players";
 
 import { useState, useEffect } from "react";
 
-export default function PlayersPresentation({players}) {
-    const[player, setPlayer] = useState([]);
-    const[showPopup, setShowPopup] = useState(false);
+export default function PlayersPresentation({ players }) {
+  const [player, setPlayer] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
 
-    // const[showPopup, setShowPopup] = useState(false);
+  // const[showPopup, setShowPopup] = useState(false);
 
-    // useEffect(()=>{
-       
-       
-    // },[showPopup]);
+  // useEffect(()=>{
 
-    const hidePopup = () => {
-        setShowPopup(false);
-      
-        if(!showPopup) window.location.href = "/#";
-    }
+  // },[showPopup]);
 
+  const hidePopup = () => {
+    setShowPopup(false);
 
-        return (
+    if (!showPopup) window.location.href = "/#";
+  };
 
-            <div onClick={hidePopup} className="container player" >
-                <a href="#"></a>
-                 <div className="row row-content">
-                 {players.map((player) => (
-                       
-                            <div className="col col-sm-4 col-md-4" key={player.id}>
-                                <div className="card">
-                                    <img src={player.img} />
-                                    <h3>{player.name}</h3>
-                                    <p className="title">{player.club}</p>
-                                    <p><button onClick={()=>{setPlayer(player);setShowPopup(true)}} className="btn btn-dark btn-lg btn-block">
-                                        <a href="#popup" id="openPopup">Detail</a></button></p>
-                                </div>
-                            </div>
-                ))}
-
-                <div id="popup" className="overlay">
-                    <div className="popup">
-                        <img src={player.img}/>
-                        <h2>{player.name}</h2>
-                        <a className="close" href="#">&times;</a>
-                        <div className="content">
-                            {player.info}
-                        </div>
-                    </div>
-
-
-                </div>
-
-
-
-                 </div>
-               
-
-
-
+  return (
+    <div onClick={hidePopup} className="container player">
+      <a href="#"></a>
+      <div className="row row-content">
+        {players.map((player) => (
+          <div className="col col-sm-4 col-md-4" key={player.id}>
+            <div className="card">
+              <img src={player.img} />
+              <h3>{player.name}</h3>
+              <p className="title">{player.club}</p>
+              <p>
+                <button
+                  onClick={() => {
+                    setPlayer(player);
+                    setShowPopup(true);
+                  }}
+                  className="btn btn-dark btn-lg btn-block"
+                >
+                  <a href="#popup" id="openPopup">
+                    Detail
+                  </a>
+                </button>
+              </p>
             </div>
-        )
+          </div>
+        ))}
 
-
+        <div id="popup" className="overlay">
+          <div className="popup">
+            <img src={player.img} />
+            <h2>{player.name}</h2>
+            <a className="close" href="#">
+              &times;
+            </a>
+            <div className="content">{player.info}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
