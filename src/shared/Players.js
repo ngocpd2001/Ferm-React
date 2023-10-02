02caa83014a487940ec2ebb649fe23cb53d4c4ab
ngocpd2001 <phanngoc2213@gmail.com>
2023-10-02 13:46:58 +0700

    halland

--- src/shared/Players.js
+++ src/shared/Players.js
@@ -1,53 +1,46 @@
-
-const Players=[
-    {
-        id:1,
-        name:"Cristiano Ronaldo",
-        club:"MU",
-        img:"assets/images/cr.jpg",
-        info:"Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team."
-    },
-    {
-        id:2,
-        name:"Kylaa Mpappe", 
-        club:"MU",
-        img:"assets/images/m.jpg",
-        info:"Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team."
-
-    },
-    {
-        id:3,
-        name:"Haaland",
-        club:"MU",
-        img:"assets/images/haaland.jpg",
-        info:"Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team."
-
-    },
-    {
-        id:4,
-        name:"Cristiano Ronaldo", 
-        club:"MU",
-        img:"assets/images/cr.jpg",
-        info:"Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team."
-
-    },
-    {
-        id:5,
-        name:"Kylaa Mpappe",
-         club:"MU",
-         img:"assets/images/m.jpg",
-         info:"Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team."
-
-    },
-    {
-        id:6,
-        name:"Haaland", 
-        club:"MU",
-        img:"assets/images/haaland.jpg",
-        info:"Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team."
-
-    }
-
+const Players = [
+  {
+    id: 1,
+    name: "Cristiano Ronaldo",
+    club: "MU",
+    img: "assets/images/cr.jpg",
+    info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
+  },
+  {
+    id: 2,
+    name: "Kylaa Mpappe",
+    club: "MU",
+    img: "assets/images/m.jpg",
+    info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
+  },
+  {
+    id: 3,
+    name: "Haaland",
+    club: "MU",
+    img: "assets/images/haaland.jpg",
+    info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
+  },
+  {
+    id: 4,
+    name: "Cristiano Ronaldo",
+    club: "MU",
+    img: "assets/images/cr.jpg",
+    info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
+  },
+  {
+    id: 5,
+    name: "Kylaa Mpappe",
+    club: "MU",
+    img: "assets/images/m.jpg",
+    info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
+  },
+  {
+    id: 6,
+    name: "Haaland 123git ",
+    club: "MU",
+    img: "assets/images/haaland.jpg",
+    info: "Cristiano Ronaldo dos Santos Aveiro was born 5 February 1985 is a Portuguese professional footballer who plays as a forward for Premier League club Manchester United and captains the Portugal national team.",
+  },
 ];
 
 export default Players;
